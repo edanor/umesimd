af3445b03d3466c0f453ee8f302916def7a51714
Przemysław Karpiński <przemyslaw.karpinski@cern.ch>
2016-08-22 19:08:04 +0200

    Add generalized, vectorized implementation for LOG/MLOG.
    Former-commit-id: 8b2c0c273e4cd09ae1e92b78e79e70130e1ea49f

--- UMESimdVectorEmulation.h
+++ UMESimdVectorEmulation.h
@@ -172,8 +172,204 @@ namespace SIMD
             t0.assign(mask, t1);
             return t0;
         }
+        // LOG - single precision
+        template<typename FLOAT_VEC_T, typename UINT_VEC_T>
+        UME_FORCE_INLINE FLOAT_VEC_T logf(FLOAT_VEC_T const & initial_x) {
+            const float MAXNUMF = 3.4028234663852885981170418348451692544e38f;
+            const float LOGF_UPPER_LIMIT = MAXNUMF;
+            const float LOGF_LOWER_LIMIT = 0;
+            const float PX1logf = 7.0376836292E-2f;
+            const float PX2logf	= -1.1514610310E-1f;
+            const float PX3logf	= 1.1676998740E-1f;
+            const float PX4logf	= -1.2420140846E-1f;
+            const float PX5logf	= 1.4249322787E-1f;
+            const float PX6logf	= -1.6668057665E-1f;
+            const float PX7logf	= 2.0000714765E-1f;
+            const float PX8logf	= -2.4999993993E-1f;
+            const float PX9logf	= 3.3333331174E-1f;
+            const float SQRTHF = 0.707106781186547524f;
 
-        // LOG
+            FLOAT_VEC_T fe;
+            //x = details::getMantExponentf( x, fe);
+            ///////////////
+            alignas(FLOAT_VEC_T::alignment()) float raw[FLOAT_VEC_T::length()];
+            initial_x.storea(raw);
+            UINT_VEC_T n;
+            n.loada((uint32_t*)&raw[0]);
+            UINT_VEC_T e = (n >> 23)-127;
+            fe = FLOAT_VEC_T(e);
+
+            // fractional part
+            const uint32_t p05f = 0x3f000000; // //sp2uint32(0.5);
+            n &= 0x807fffff;// ~0x7f800000;
+            n |= p05f;
+
+            n.storea((uint32_t*)&raw[0]);
+            FLOAT_VEC_T x;
+            x.loada(raw);
+            //////////////
+
+            fe.postinc(x > SQRTHF);
+            x.adda(x <= SQRTHF, x);
+            x -= 1.0f;
+
+            const FLOAT_VEC_T x2 = x*x;
+
+            //FLOAT_VEC_T res = details::get_log_poly(x);
+            FLOAT_VEC_T res = x*PX1logf;
+            res += PX2logf;
+            res *= x;
+            res += PX3logf;
+            res *= x;
+            res += PX4logf;
+            res *= x;
+            res += PX5logf;
+            res *= x;
+            res += PX6logf;
+            res *= x;
+            res += PX7logf;
+            res *= x;
+            res += PX8logf;
+            res *= x;
+            res += PX9logf;
+
+            res *= x2*x;
+
+            res += -2.12194440e-4f * fe;
+            res +=  -0.5f * x2;
+
+            res= x + res;
+
+            res += 0.693359375f * fe;
+
+            res[initial_x > LOGF_UPPER_LIMIT] = std::numeric_limits<float>::infinity();
+            res[initial_x < LOGF_LOWER_LIMIT] = -std::numeric_limits<float>::quiet_NaN();
+
+            return res;
+        }
+        // LOG - double precision
+        template<typename FLOAT_VEC_T, typename UINT_VEC_T>
+        UME_FORCE_INLINE FLOAT_VEC_T logd(FLOAT_VEC_T const & initial_x) {
+            const double LOG_UPPER_LIMIT = 1e307;
+            const double LOG_LOWER_LIMIT = 0;
+
+            const double SQRTH = 0.70710678118654752440;
+
+            /* separate mantissa from exponent */
+            FLOAT_VEC_T fe;
+            //x = details::getMantExponent(x,fe);
+            ///////
+            alignas(FLOAT_VEC_T::alignment()) double raw[FLOAT_VEC_T::length()];
+            initial_x.storea(raw);
+            UINT_VEC_T n;
+            n.loada((uint64_t*)&raw[0]);
+
+            // Shift to the right up to the beginning of the exponent.
+            // Then with a mask, cut off the sign bit
+            UINT_VEC_T le = (n >> 52);
+
+            // chop the head of the number: an int contains more than 11 bits (32)
+            UINT_VEC_T e = le;
+            fe = e-1023;
+
+            // This puts to 11 zeroes the exponent
+            n &=0x800FFFFFFFFFFFFFULL;
+            // build a mask which is 0.5, i.e. an exponent equal to 1022
+            // which means *2, see the above +1.
+            const uint64_t p05 = 0x3FE0000000000000ULL; //dp2uint64(0.5);
+            n |= p05;
+
+            n.storea((uint64_t*)&raw[0]);
+            FLOAT_VEC_T x;
+            x.loada(&raw[0]);
+
+            ///////
+            // blending
+            fe.postinc(x > SQRTH);
+            x.adda(x <= SQRTH, x);
+            x -= 1.0;
+
+            /* rational form */
+            //FLOAT_VEC_T px =  details::get_log_px(x);
+            //////
+            const double PX1log = 1.01875663804580931796E-4;
+            const double PX2log = 4.97494994976747001425E-1;
+            const double PX3log = 4.70579119878881725854E0;
+            const double PX4log = 1.44989225341610930846E1;
+            const double PX5log = 1.79368678507819816313E1;
+            const double PX6log = 7.70838733755885391666E0;
+
+            FLOAT_VEC_T px(PX1log);
+            px *= x;
+            px += PX2log;
+            px *= x;
+            px += PX3log;
+            px *= x;
+            px += PX4log;
+            px *= x;
+            px += PX5log;
+            px *= x;
+            px += PX6log;
+
+            /////
+
+            //for the final formula
+            const FLOAT_VEC_T x2 = x*x;
+            px *= x;
+            px *= x2;
+
+            //const FLOAT_VEC_T qx = details::get_log_qx(x);
+            //////
+
+            const double QX1log = 1.12873587189167450590E1;
+            const double QX2log = 4.52279145837532221105E1;
+            const double QX3log = 8.29875266912776603211E1;
+            const double QX4log = 7.11544750618563894466E1;
+            const double QX5log = 2.31251620126765340583E1;
+
+            FLOAT_VEC_T qx = x;
+            qx += QX1log;
+            qx *=x;
+            qx += QX2log;
+            qx *=x;
+            qx += QX3log;
+            qx *=x;
+            qx += QX4log;
+            qx *=x;
+            qx += QX5log;
+            /////
+
+            FLOAT_VEC_T res = px / qx ;
+
+            res -= fe * 2.121944400546905827679e-4;
+            res -= 0.5 * x2  ;
+
+            res = x + res;
+            res += fe * 0.693359375;
+
+            res[initial_x > LOG_UPPER_LIMIT] = std::numeric_limits<double>::infinity();
+            res[initial_x < LOG_LOWER_LIMIT] = -std::numeric_limits<double>::quiet_NaN();
+
+            return res;
+
+        }
+        // MLOG - single precision version
+        template<typename FLOAT_VEC_T, typename UINT_VEC_T, typename MASK_T>
+        UME_FORCE_INLINE FLOAT_VEC_T logf(MASK_T const & mask, FLOAT_VEC_T const & initial_x) {
+            FLOAT_VEC_T t0 = initial_x;
+            FLOAT_VEC_T t1 = logf<FLOAT_VEC_T, UINT_VEC_T>(initial_x);
+            t0.assign(mask, t1);
+            return t0;
+        }
+        // MLOG - double precision version
+        template<typename FLOAT_VEC_T, typename UINT_VEC_T, typename MASK_T>
+        UME_FORCE_INLINE FLOAT_VEC_T logd(MASK_T const & mask, FLOAT_VEC_T const & initial_x) {
+            FLOAT_VEC_T t0 = initial_x;
+            FLOAT_VEC_T t1 = logd<FLOAT_VEC_T, UINT_VEC_T>(initial_x);
+            t0.assign(mask, t1);
+            return t0;
+        }
+    
         // LOG2
         // LOG10
 
